0a9038df4730bbdac37033f72a057dbcc76ce21e
Jabb17 <javierbarrientos2010@gmail.com>
2022-03-08 17:04:12 +0100

    Added new dependencies pom.xml

--- Api/nearme/src/main/java/com/nearme/nearme/NearmeApplication.java
+++ Api/nearme/src/main/java/com/nearme/nearme/NearmeApplication.java
@@ -2,13 +2,10 @@ package com.nearme.nearme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class NearmeApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(NearmeApplication.class, args);
 	}
