0849b2204cd495bc1f14d171da809f610e017338
Jabb17 <javierbarrientos2010@gmail.com>
2022-03-10 16:00:18 +0100

    Creación componente home y login

--- NearMeClient/src/app/app-routing.module.ts
+++ NearMeClient/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './homeComponent/home.component';
+import { LoginComponent } from './loginComponent/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- NearMeClient/src/app/app.module.ts
+++ NearMeClient/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './loginComponent/login.component';
+import { HomeComponent } from './homeComponent/home.component';
+
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
