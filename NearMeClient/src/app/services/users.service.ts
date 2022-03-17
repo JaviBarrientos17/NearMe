df2fc71d80978a217790c67be78bf1e3d09ea85c
Jabb17 <javierbarrientos2010@gmail.com>
2022-03-17 16:01:19 +0100

    Backend form login y registro

---
+++ NearMeClient/src/app/services/users.service.ts
@@ -0,0 +1,9 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class UsersService{
+    constructor(private conexHttp:HttpClient){}
+
+    // TODO CREAR FUNCIONES QUE APUNTEN A LA BBDD
+}
