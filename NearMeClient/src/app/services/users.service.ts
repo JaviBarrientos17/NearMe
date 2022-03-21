440e44e8abaa89d823886e944f63becc776db1c9
Jabb17 <javierbarrientos2010@gmail.com>
2022-03-21 16:05:47 +0100

    Funciona endpoint fake getAllUsers

--- NearMeClient/src/app/services/users.service.ts
+++ NearMeClient/src/app/services/users.service.ts
@@ -1,36 +1,14 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class UsersService{
+    url:string = "https://jsonplaceholder.typicode.com/users";
+
     constructor(private conexHttp:HttpClient){}
 
-    // TODO CREAR FUNCIONES QUE APUNTEN A LA BBDD
-
-    selectAllUsers():Observable<any>{
-        return this.conexHttp.get(
-            "",
-            {headers:new HttpHeaders(
-                {'Content-Type':'application/json'})
-            }
-        );
-    }
-
-    insertNewUser():Observable<any>{
-        let formData:FormData = new FormData;
-
-        return this.conexHttp.post(
-            "", formData
-        );
-    }
-
-    getUser():Observable<any>{
-        return this.conexHttp.get(
-            "",
-            {headers:new HttpHeaders(
-                {'Content-Type':'application/json'})
-            }
-        );
+    getAllUsers():Observable<any> {
+        return this.conexHttp.get(this.url);
     }
 }
