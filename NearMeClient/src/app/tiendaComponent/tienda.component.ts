ecfdd753d34bb7f20dc49842f1f69a99a57d3702
Javi Barrientos <javierbarrientos2010@gmail.com>
2022-04-22 16:54:55 +0200

    Arreglando back login y haciendo front tienda

---
+++ NearMeClient/src/app/tiendaComponent/tienda.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './tienda.component.html',
+  styleUrls: ['./tienda.component.css']
+})
+export class TiendaComponent {
+  title = 'Tienda';
+}
