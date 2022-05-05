ca1651ef14f9efcb31d20b74ff464bcade8d6bea
Aleix Blanch <aleix@inovum-solutions.com>
2022-05-05 18:24:19 +0200

    fixed

--- Api/blog-spring-boot-swagger-master/src/main/java/com/nearme/mappers/ImageMapper.java
+++ Api/blog-spring-boot-swagger-master/src/main/java/com/nearme/mappers/ImageMapper.java
@@ -1,21 +1,21 @@
 package com.nearme.mappers;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import com.nearme.models.dto.ImageDTO;
-import com.nearme.models.entities.ImageEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+// import java.util.ArrayList;
+// import java.util.List;
+// import com.nearme.models.dto.ImageDTO;
+// import com.nearme.models.entities.ImageEntity;
+// import org.mapstruct.Mapper;
+// import org.mapstruct.factory.Mappers;
 
-@Mapper
+// @Mapper
 public interface ImageMapper {
-    ImageMapper INSTANCE = Mappers.getMapper(ImageMapper.class);
+    // ImageMapper INSTANCE = Mappers.getMapper(ImageMapper.class);
 
     
-    ImageDTO entityToDto(ImageEntity entity);
+    // ImageDTO entityToDto(ImageEntity entity);
 
-    ImageEntity dtoToEntity(ImageDTO dto);
+    // ImageEntity dtoToEntity(ImageDTO dto);
 
-    ArrayList<ImageDTO> mapEntityToDtoList(List<ImageEntity> userEntityList);
+    // ArrayList<ImageDTO> mapEntityToDtoList(List<ImageEntity> userEntityList);
 }
