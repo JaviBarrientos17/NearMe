e5f9101b5e6cff6aa6e4f61180e1e2ddee17a21b
Javi Barrientos <javierbarrientos2010@gmail.com>
2022-05-13 16:55:38 +0200

    Working shopping cart

--- NearMeClient/src/app/shoppingCart/shoppingCart.component.ts
+++ NearMeClient/src/app/shoppingCart/shoppingCart.component.ts
@@ -1,6 +1,7 @@
 import { Component, Injectable } from '@angular/core';
 import { BaseCartItem, CheckoutPaypalSettings } from 'ng-shopping-cart';
 import { cartService } from 'src/app/services/cart.service';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,9 @@ import { cartService } from 'src/app/services/cart.service';
 export class ShoppingCartComponent {
   title = 'ShoppingCart';
   cartItems = new BaseCartItem();
+  total = 0;
+  idProduct:any = 1;
 
-  // create array of BaseCartItem
   cartItemsArray = [
     new BaseCartItem({
       id: this.cartItems.getId(),
@@ -24,16 +26,39 @@ export class ShoppingCartComponent {
     }),
   ];
 
+  constructor(private cartService: cartService, private productService: ProductsService) {}
+
   ngOnInit(): void {
     // TODO SETEAR AL ITEM LOS VALORES DE LA BBDD
     // TODO PASAR TODO LO DE LA LIBRERIA DEL SHOPPING CART EN EL MODELO ITEM AL MODELO PRODUCTO
     
-    this.cartItems.setName("TestItem");
-    this.cartItems.setPrice(10);
-    this.cartItems.setQuantity(1);
-    this.cartItems.setImage("https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500");
-    this.cartItems.setId(1);
-    
-    console.log(JSON.parse(JSON.stringify(this.cartItems)));
+    // this.cartItems.setName("TestItem");
+    // this.cartItems.setPrice(10);
+    // this.cartItems.setQuantity(1);
+    // this.cartItems.setImage("https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500");
+    // this.cartItems.setId(1);
+
+    // TODO COMPROBAR SI RECIBE LOS DATOS DEL PRODUCTO
+    this.productService.getProductsById(this.idProduct).subscribe(
+      (data) => {
+        // this.cartItems.setName(
+        //   this.productService.getProductsById(this.idProduct: Number).subscribe(
+        //     (data) => {
+        //     },
+        //     (error) => {
+        //       console.log(error);
+        //     },
+        //   )
+        // );
+
+        console.log(JSON.parse(JSON.stringify(this.cartItems)));
+
+        // TODO FUNCIONA, PERO LOS DATOS LOS DEBE RECIBIR DE LA BBDD
+        this.total = this.cartItems.getPrice() * this.cartItems.getQuantity();
+        console.log(this.total);
+      },(error) => {
+        console.log(error);
+      }
+    );
   }
 }
