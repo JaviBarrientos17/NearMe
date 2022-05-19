4293f7bce223c8dcadefb93519f992e5c7cc265e
Aleix Blanch <aleix@inovum-solutions.com>
2022-05-19 15:32:34 +0200

    master-stable

--- Api/blog-spring-boot-swagger-master/src/main/java/com/nearme/models/dto/AuthenticationRequestDTO.java
+++ Api/blog-spring-boot-swagger-master/src/main/java/com/nearme/models/dto/AuthenticationRequestDTO.java
@@ -1,8 +1,10 @@
 package com.nearme.models.dto;
-import lombok.*;
-import javax.validation.constraints.NotNull;
-import io.swagger.v3.oas.annotations.media.Schema;
 
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 @Builder
