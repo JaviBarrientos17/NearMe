c00532229ead0359a192019451fe0674b93bb10d
Javi Barrientos <javierbarrientos2010@gmail.com>
2022-05-28 13:18:36 +0200

    Added categories images to homepage

--- NearMeClient/src/model/category.ts
+++ NearMeClient/src/model/category.ts
@@ -3,11 +3,13 @@ export class Category {
   name: String;
   parent: Number;
   root: boolean;
+  imgUrl: String;
 
-  constructor(idCategory: String, name: string, parent: Number, root: boolean) {
+  constructor(idCategory: String, name: string, parent: Number, root: boolean, imgUrl: String) {
     this.idCategory = idCategory;
     this.name = name;
     this.parent = parent;
     this.root = root;
+    this.imgUrl = imgUrl;
   }
 }
