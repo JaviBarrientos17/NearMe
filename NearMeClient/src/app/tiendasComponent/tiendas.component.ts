f7f27f993603628f8b6ef0f16b8e7671f23847a9
Javi Barrientos <javierbarrientos2010@gmail.com>
2022-05-29 00:03:31 +0200

    Arreglado provider tiendas component

--- NearMeClient/src/app/tiendasComponent/tiendas.component.ts
+++ NearMeClient/src/app/tiendasComponent/tiendas.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Tienda } from 'src/model/tienda';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './tiendas.component.html',
   styleUrls: ['./tiendas.component.css'],
+  providers: [ProductsService],
 })
 export class TiendasComponent {
   title = 'Tiendas';
